ea3c30fb2245c77952d568fa79b8171dfaf0030f
svalencia014 <sv.samuel.valencia014@gmail.com>
2026-01-03 15:42:10 -0500

    idk

--- PointOfSale.AppHost/AppHost.cs
+++ PointOfSale.AppHost/AppHost.cs
@@ -1,6 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var api = builder.AddProject<Projects.PointOfSale_Api>("api")
+  .WithHttpHealthCheck("/health");
+
 var web = builder.AddProject<Projects.PointOfSale_Web>("web")
   .WithHttpHealthCheck("/health");
+
 
 builder.Build().Run();
