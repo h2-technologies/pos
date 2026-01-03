5d2ea84c6eb7f9709cfc6744c7d5786d222eea96
svalencia014 <sv.samuel.valencia014@gmail.com>
2026-01-03 14:31:43 -0500

    remove extra stuff

--- src/routes/login/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad, RequestEvent } from "./$types";
-
-export const load: PageServerLoad = (event: RequestEvent) => {
-  if (event.locals.session != null) {
-    return redirect(302, '/');
-  }
-}
